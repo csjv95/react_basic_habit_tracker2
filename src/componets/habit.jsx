1a300ecb0845d3c8fe32280c4d61a2378b7ac0e3
csjv95 <csjv95@gmail.com>
2021-01-30 23:30:19 +0900

    Create Habits component and it's have pops(key,habit....) and get onclick (finished plus)

--- src/componets/habit.jsx
+++ src/componets/habit.jsx
@@ -1,42 +1,32 @@
-import React, { Component } from 'react';
-import './habit.css';
+import React, { Component } from "react";
+import "./habit.css";
 
 class Habit extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count : 0
-    }
-  }
-  
-  increment = ()=> {
-    const count = this.state.count + 1;
-    this.setState({count});
-  }
-  
-  decrement = ()=> {
-    const count = this.state.count - 1;
-    this.setState({count : count >= 0 ? count : 0});
-    console.log({count});
-  }
+  increment = () => {
+    return this.props.handleIncrement(this.props.habit);
+  };
 
-  delete = ()=> {
-    console.log('delete');
-  }
+  decrement = () => {
+    console.log("decrement");
+  };
+
+  delete = () => {
+    console.log("delete");
+  };
   render() {
-    const {name , count} = this.props.habit;
+    const { name, count } = this.props.habit;
 
     return (
-      <li className='habit'>
-        <span className='habit_name'>{name}</span>
-        <span className='habit_count'>{count}</span>
-        <button className='habit_btn habit_increase' onClick={this.increment}>
+      <li className="habit">
+        <span className="habit_name">{name}</span>
+        <span className="habit_count">{count}</span>
+        <button className="habit_btn habit_increase" onClick={this.increment}>
           <i className="fas fa-plus"></i>
         </button>
-        <button className='habit_btn habit_decrease' onClick={this.decrement}>
+        <button className="habit_btn habit_decrease" onClick={this.decrement}>
           <i className="fas fa-minus"></i>
         </button>
-        <button className='habit_btn habit_delete' onClick={this.delete}>
+        <button className="habit_btn habit_delete" onClick={this.delete}>
           <i className="fas fa-trash"></i>
         </button>
       </li>
--- src/componets/habits.jsx
+++ src/componets/habits.jsx
@@ -5,7 +5,7 @@ class Habits extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      habit : [
+      habits : [
         {id : 1, name : 'Jogging', count : 0 },
         {id : 2, name : 'cooking', count : 0 },
         {id : 3, name : 'studying', count : 0 }
@@ -13,12 +13,33 @@ class Habits extends Component {
       ]
     }
   }
+
+  handleIncrement= (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({habit : habit})
+  }
+
+  handleDecrement = () => {
+
+  }
+
+  handleDelete = () => {
+
+  }
   
   render() {
-    const habit = this.state.habit;
+    const habits = this.state.habits;
     return (
       <ul>
-        {habit.map(habit => <Habit key={habit.id} habit={habit}/>)}
+        {habits.map(habit => <Habit 
+                              key={habit.id}
+                              habit={habit}
+                              handleIncrement={this.handleIncrement}
+                              handleDecrement={this.handleDecrement}
+                              handleDelete={this.handleDelete}
+                            />)}
       </ul>
     );
   }
