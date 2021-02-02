ec60add38af0c4d4c5b8d51537900f6b4e8fe4b4
csjv95 <csjv95@gmail.com>
2021-02-02 18:21:11 +0900

    create static input component

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,13 @@
+import Nav from "./componets/nav/nav";
+import Input from "./componets/input/input";
 import Habits from "./componets/habits";
 import "./app.css";
-import Nav from "./componets/nav/nav";
 
 function App() {
   return (
     <main>
       <Nav />
+      <Input />
       <Habits />
     </main>
   );
---
+++ src/componets/input/input.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Input extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" />
+      </form>
+    );
+  }
+}
+
+export default Input;
